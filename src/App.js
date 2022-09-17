6202cd37ce23565f22abb1056ba813e3276b82cf
NingBo Wang <2536935847@qq.com>
2022-09-17 22:50:22 +0800

    nbchain kitties

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
 } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 
+
 import { SubstrateContextProvider, useSubstrateState } from './substrate-lib'
 import { DeveloperConsole } from './substrate-lib/components'
 
